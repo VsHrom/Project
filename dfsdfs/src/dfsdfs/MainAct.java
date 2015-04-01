589c074f60f3e88992d69e69bb43fe591f12f89f
VsHrom <dudnikvad@gmail.com>
2015-04-01 18:58:35 +0200

    asdasd

---
+++ dfsdfs/src/dfsdfs/MainAct.java
@@ -0,0 +1,13 @@
+package dfsdfs;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAct extends Activity {
+	protected void OnCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.glowna);
+	}
+}
